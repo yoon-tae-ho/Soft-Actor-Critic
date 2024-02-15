1d8c78b9339c8651968c92095edaf517d9e7514f
yoontaeho <pln0302@yonsei.ac.kr>
2024-02-16 00:56:06 +0900

    Fixed indentation

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from mlagents_envs.side_channel.engine_configuration_channel import EngineConfig
 from arg_parser import args
 
 # envs
-file_name = "subway100"
+file_name = "subway103"
 env_path = f"./{file_name}"
 
 # #############################################################################
@@ -19,24 +19,24 @@ train_mode = True
 max_episode_steps = 1000000
 
 def env_reset(env, behavior_name):
-  env.reset()
-  dec, term = env.get_steps(behavior_name)
-  done = len(term.agent_id) > 0
-  state = term.obs[0] if done else dec.obs[0]
-  return state
+    env.reset()
+    dec, term = env.get_steps(behavior_name)
+    done = len(term.agent_id) > 0
+    state = term.obs[0] if done else dec.obs[0]
+    return state
 
 def env_next_step(env, behavior_name, action):
-  action_tuple = ActionTuple()
-  action_tuple.add_continuous(action)
-  env.set_actions(behavior_name, action_tuple)
-  env.step()
-  
-  dec, term = env.get_steps(behavior_name)
-  done = len(term.agent_id) > 0
-  reward = term.reward if done else dec.reward
-  next_state = term.obs[0] if done else dec.obs[0]
-  
-  return next_state, reward, done
+    action_tuple = ActionTuple()
+    action_tuple.add_continuous(action)
+    env.set_actions(behavior_name, action_tuple)
+    env.step()
+    
+    dec, term = env.get_steps(behavior_name)
+    done = len(term.agent_id) > 0
+    reward = term.reward if done else dec.reward
+    next_state = term.obs[0] if done else dec.obs[0]
+    
+    return next_state, reward, done
 
 if __name__ == "__main__":
     # CUDA
@@ -138,6 +138,7 @@ if __name__ == "__main__":
             print("----------------------------------------")
             print("Test Episodes: {}, Avg. Reward: {}".format(episodes, round(avg_reward[0], 2)))
             print("----------------------------------------")
+            
 
     env.close()
 
